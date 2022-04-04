91933597d1ad25c201c40563109202dd6cb61c06
Mauricio Edson de Souza <mauricioesdonsouza@gmail.com>
2022-04-04 17:33:14 -0300

    iniciando aula if-eslse-switch-case

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,11 +5,6 @@ function App() {
 const mensagem = 'é muito bom';
 const doce =  'chocolate';
 
-    // cria uma função externa que chame o numero digitado no input no console.log acrtescentando 
-    // a mensagem --é muito louco--
-
-    //passar duas variáveis para uma segunda função onde elas retornem dentro de um alert depois do click
-
     return (
         <form>
             <input onChange={(e) => Mercado(e.target.value)} />
