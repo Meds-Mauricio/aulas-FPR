d6f3b819657868c14be22f87fcb3fa89cd3dd415
Mauricio Edson de Souza <mauricioesdonsouza@gmail.com>
2022-04-27 16:44:13 -0300

    exercicio aula presencial 8 useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,8 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Lojas from './pages/loja';
-import Home from './pages/home';
+import React from 'react'
+import Home from './pages/home'
 
-function App() {
-    return (
-        <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/lojas' element={<Lojas />} />
-        </Routes>
-      </BrowserRouter>
-    )
+export default function App() {
+  return (
+      <Home />
+  )
 }
-export default App;
--- my-app/src/pages/home/index.js
+++ my-app/src/pages/home/index.js
@@ -1,24 +1,81 @@
-import React from "react"
+import React, { useState } from 'react';
 
 function Home() {
-    let valor
-    const shopping = (lojas) => {
-        valor = lojas
+  const [pergunta1, setPergunta1] = useState()
+  const [pergunta2, setPergunta2] = useState()
+  const [pergunta3, setPergunta3] = useState()
+  const [pergunta4, setPergunta4] = useState()
+  const [pergunta5, setPergunta5] = useState()
+  const [resposta, setResposta] = useState(0)
+  const [step, setStep] = useState(0)
+
+  const um = pergunta1 === 'poly'
+  const dois = pergunta2 === 'kaio'
+  const tres = pergunta3 === 'emanuel'
+  const quatro = pergunta4 === 'gelinho'
+  const cinco = pergunta5 === 'marcelo'
+
+  const resultado = (nivel) => {
+    setStep(step + 1)
+    if (nivel) {
+      setResposta(resposta + 1)
     }
-    const click = () => {
-        window.location.href = `/lojas?=${valor}`
+  }
+
+  const final = () => {
+    switch (resposta) {
+      case 0: return 'horrivel';
+      case 1: return 'muito ruim';
+      case 2: return 'ruim';
+      case 3: return 'mediano';
+      case 4: return 'bom';
+      case 5: return 'muito bom';
     }
-    return (
-        <div className="box">
-            <form>
-                <input onChange={(e) => shopping(e.target.value)} />
-                <button onClick={(e) => {
-                    e.preventDefault()
-                    click()
-                }}
-                >teste</button>
-            </form>
+  }
+  return (
+    <>
+      {step === 0 &&
+        <div>
+          <label>Quem é o mais velho?</label>
+          <input onChange={(e) => { setPergunta1(e.target.value) }} />
+          <button onClick={() => { resultado(um) }}>Responder</button>
         </div>
-    )
+      }
+
+
+      {step === 1 &&
+        <div>
+          <label>Quem é o mais chato?</label>
+          <input onChange={(e) => { setPergunta2(e.target.value) }} />
+          <button onClick={() => { resultado(dois) }}>Responder</button>
+        </div>
+      }
+      {step === 2 &&
+        <div>
+          <label>Quem é o mais chorão?</label>
+          <input onChange={(e) => { setPergunta3(e.target.value) }} />
+          <button onClick={() => { resultado(tres) }}>Responder</button>
+        </div>
+      }
+      {step === 3 &&
+        <div>
+          <label>Quem é o mais alto?</label>
+          <input onChange={(e) => { setPergunta4(e.target.value) }} />
+          <button onClick={() => { resultado(quatro) }}>Responder</button>
+        </div>
+      }
+      {step === 4 &&
+        <div>
+          <label>Quem é o mais novo?</label>
+          <input onChange={(e) => { setPergunta5(e.target.value) }} />
+          <button onClick={() => { resultado(cinco) }}>Responder</button>
+        </div>
+      }
+      {step === 5 &&
+        <p>Você foi {final()}</p>
+      }
+    </>
+  )
 }
+
 export default Home;
